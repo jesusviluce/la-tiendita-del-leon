11464783c04e74a944ada060d98a29a4afd488ec
jesusviluce <jesusvilucemarin047@gmail.com>
2026-02-12 23:53:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let total = 0;
+const carrito = document.getElementById("carrito");
+const mensajeVacio = document.getElementById("mensaje-vacio");
+const totalTexto = document.getElementById("total");
+
+const botones = document.querySelectorAll(".helado button");
+
+botones.forEach(function(boton) {
+    boton.addEventListener("click", function() {
+        const helado = this.parentElement;
+        const nombre = helado.querySelector("h3").textContent;
+        const precioTexto = helado.querySelector("p").textContent;
+        const precio = parseFloat(precioTexto.replace("Precio: $", ""));
+
+        total += precio;
+mensajeVacio.style.display = "none";
+
+        carrito.insertAdjacentHTML("beforeend", `<p>${nombre} - $${precio.toFixed(2)}</p>`);
+        totalTexto.textContent = "Total: $" + total.toFixed(2);
+    });
+});
+
+const finalizar = document.getElementById("finalizar");
+const vaciar = document.getElementById("vaciar");
+finalizar.addEventListener("click", function() {
+    if (total === 0) {
+        alert("Tu carrito está vacío 😅");
+        return;
+    }
+
+    let mensaje = "Hola, quiero pedir:%0A";
+
+    const items = carrito.querySelectorAll("p");
+    items.forEach(function(item) {
+        mensaje += item.textContent + "%0A";
+    });
+
+    mensaje += "%0ATotal: $" + total.toFixed(2);
+
+    const numero = "50765217752";
+    const url = "https://web.whatsapp.com/send?phone=" + numero + "&text=" + mensaje;
+
+    window.open(url, "_blank");
+});
+vaciar.addEventListener("click", function() {
+    carrito.innerHTML = "";
+    total = 0;
+    totalTexto.textContent = "Total: $0.00";
+    mensajeVacio.style.display = "block";
+});xx
